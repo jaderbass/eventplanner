b2392cd5d3025172194a3ba18e952291c353e148
jaderbass <joerg@jaderbass.de>
2026-09-07 14:49:34 +0200

    feat: add more properties and data-types number and boolean

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -4,6 +4,10 @@ export default function EventCard(props) {
       <h2> { props.title } </h2>
       <p> { props.date } </p>
       <p> { props.location } </p>
+      <p> Trainer: { props.speaker } </p>
+      <p> Preis: { props.price } € </p>
+      <p> { props.online } </p>
+
       <button>Details</button>
     </article>
   );
--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -7,24 +7,36 @@ export default function EventList() {
         title="React Grundlagen"
         date="12. September 2026"
         location="Raum 1"
+        speaker="Anna Müller"
+        price={ 129 }
+        online={ false }
       />
 
       <EventCard 
         title="JavaScript Workshop"
         date="19. September 2026"
         location="Raum 2"
+        speaker="Robert Rabe"
+        price={ 149 }
+        online={ true }
       />
       
       <EventCard 
         title="HTML/CSS Grundlagen"
         date="12. September 2026"
         location="Raum 2"
+        speaker="Heribert Hüller"
+        price={ 109 }
+        online={ true }
       />
 
       <EventCard 
         title="PHP Grundlagen"
         date="26. September 2026"
         location="Raum 5"
+        speaker="Klausbernd Kummer"
+        price={ 169 }
+        online={ false }
       />
     </section>
   );
